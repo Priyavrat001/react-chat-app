a47e7c1fef685616b052e6b75e07059c9b8eaf85
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2023-12-28 18:21:11 +0530

    Added side navbar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { useTheme } from "@emotion/react";
 import { Outlet } from "react-router-dom";
+import { Box, IconButton, Stack, Divider } from "@mui/material";
+import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons } from "../../data"
+import { Gear } from "phosphor-react";
+import { useState } from "react";
 
 const DashboardLayout = () => {
-
+  const [selected, setSelected] = useState(0)
+  const theme = useTheme();
   return (
     <>
-      Dashboard Layout
+      <Box
+        p={2}
+        sx={{ backgroundColor: theme.palette.background.paper, height: "100vh", width: 100, boxShadow: "0px 1px 0px 1px rgba(0, 0, 0, 0.25)" }}>
+
+        <Stack direction="column" alignItems="center" sx={{ width: "100%" }} spacing={3}>
+
+
+          <Box sx={{ backgroundColor: theme.palette.primary.main, height: 64, width: 64, borderRadius: 1.5 }}>
+            <img src={Logo} alt="logoIcon" />
+
+          </Box>
+          <Stack spacing={3}
+            sx={{ width: "max-content" }}
+            direction="column"
+            alignItems="center"
+          >
+
+            {Nav_Buttons.map((elem) => {
+              return <Box>
+                <IconButton key={elem.index}>
+                  {elem.icon}
+                </IconButton>
+              </Box>
+            })}
+            <Divider />
+            <IconButton>
+              <Gear />
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );
